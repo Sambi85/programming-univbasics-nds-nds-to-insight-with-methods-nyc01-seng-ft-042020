7b1c718277757da2863169ac0c7bb41b8808f09f
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-01 03:31:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,68 +29,61 @@ total1 = 0
 row_index = 0 
 
 while row_index < nds.length do
+result = {}
 
 if nds[row_index][:name] == 'Stephen Spielberg'
-dir_name1 = nds[row_index][:name]  ### good !!! ###
-total1 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
+dir_name1 = nds[row_index][:name] 
+total1 = gross_for_director(nds[row_index])
 result.store(dir_name1, total1)
 end
 
 if nds[row_index][:name] == 'Russo Brothers'
-dir_name2 = nds[row_index][:name]  ### good !!! ###
-total2 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
+dir_name2 = nds[row_index][:name]  
+total2 = gross_for_director(nds[row_index]) 
 result.store(dir_name2, total2)
 end
 
 if nds[row_index][:name] == 'James Cameron'
-dir_name3 = nds[row_index][:name]  ### good !!! ###
-total3 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
+dir_name3 = nds[row_index][:name]  
+total3 = gross_for_director(nds[row_index])
 result.store(dir_name3, total3)
 end
 
 if nds[row_index][:name] == 'Spike Lee'
-dir_name3 = nds[row_index][:name]  ### good !!! ###
-total3 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
-result.store(dir_name3, total3)
-end
+dir_name4 = nds[row_index][:name]  
+total4 = gross_for_director(nds[row_index])
 
-if nds[row_index][:name] == 'Wachowski Brothers'
-dir_name4 = nds[row_index][:name]  ### good !!! ###
-total4 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
 result.store(dir_name4, total4)
 end
 
-if nds[row_index][:name] == 'Russo Brothers'
-dir_name2 = nds[row_index][:name]  ### good !!! ###
-total2 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
-result.store(dir_name2, total2)
+if nds[row_index][:name] == 'Wachowski Brothers'
+dir_name5 = nds[row_index][:name] 
+total5 = gross_for_director(nds[row_index]) 
+result.store(dir_name5, total5)
+end
+
+if nds[row_index][:name] == 'Robert Zemeckis'
+dir_name6 = nds[row_index][:name]
+total6 = gross_for_director(nds[row_index])
+result.store(dir_name6, total6)
 end
 
 if nds[row_index][:name] == 'Russo Brothers'
-dir_name2 = nds[row_index][:name]  ### good !!! ###
-total2 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
-result.store(dir_name2, total2)
+dir_name7 = nds[row_index][:name]
+total7 = gross_for_director(nds[row_index])
+result.store(dir_name7, total7)
+end
+
+if nds[row_index][:name] == 'Quetin Tarantino'
+dir_name8 = nds[row_index][:name]
+total8 = gross_for_director(nds[row_index])
+result.store(dir_name8, total8)
 end
 
 if nds[row_index][:name] == 'Russo Brothers'
-dir_name2 = nds[row_index][:name]  ### good !!! ###
-total2 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
-result.store(dir_name2, total2)
-end
-
-if nds[row_index][:name] == 'Russo Brothers'
-dir_name2 = nds[row_index][:name]  ### good !!! ###
-total2 = gross_for_director(nds[row_index]) ### good !!!
-result = {}
-result.store(dir_name2, total2)
+dir_name9 = nds[row_index][:name]
+total9 = gross_for_director(nds[row_index])
+result.store(dir_name9, total9)
 end
 row_index += 1
 end
